c58ff0f9e85025effd1bf63fc917715749ed8bf4
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-03-29 12:32:02 -0500

    code commit

---
+++ src/List.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const List = ({ people }) => {
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className='person   div1' >
+           <img src={image} alt={name} />
+            <div className="div2">
+            
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+};
+
+export default List;
